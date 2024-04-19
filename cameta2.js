f7ed37782c53dc2c91c373fa34d071d3832dd834
Alisson Acioli <alissonacioli@hotmail.com>
2024-04-19 11:45:29 -0300

    Add files via upload

---
+++ cameta2.js
@@ -0,0 +1,181 @@
+{
+    "polo-titulo": "Polo Cametá (Marambaia II) - PA",
+    "polo-endereco": "Travessa Enéas Martins – 464, Marambaia, Cametá - PA",
+    "polo-telefone1": "91 98522-0056",
+    "polo-telefone2": "81 98180-1386",
+    "polo-whatsapp": "https://wa.link/w4h3j4",
+    "cursos": {
+        "especializacao-em-neurociencias-e-comportamento-humano": {
+            "a-vista": "?utm=pos-aovivo-unama-cameta1&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3oemneuroci%C3%AAnciasecomportamentohumano&coupon=UNAMA5",
+            "parcelado": "?utm=pos-aovivo-unama-cameta7&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3oemneuroci%C3%AAnciasecomportamentohumano"
+        },
+        "especializacao-em-farmacia-clinica-e-atencao-farmaceutica": {
+            "a-vista": "?utm=pos-aovivo-unama-cameta4&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3oemfarm%C3%A1ciacl%C3%ADnicaeaten%C3%A7%C3%A3ofarmac%C3%AAutica&coupon=UNAMA8",
+            "parcelado": "?utm=pos-aovivo-unama-cameta10&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3oemfarm%C3%A1ciacl%C3%ADnicaeaten%C3%A7%C3%A3ofarmac%C3%AAutica"
+        },
+        "ao-vivo-especializacao-em-direito-penal-e-processo-penal": {
+            "a-vista": "?utm=pos-aovivo-unama-cameta5&utm_source=posgradunama&utm_campaign=especializaçãoemdireitopenaleprocessopenal&coupon=UNAMA9",
+            "parcelado": "?utm=pos-aovivo-unama-cameta11&utm_source=posgradunama&utm_campaign=especializaçãoemdireitopenaleprocessopenal"
+        },
+        "ao-vivo-especializacao-em-direito-do-trabalho-previdenciario": {
+            "a-vista": "?utm=pos-aovivo-unama-cameta-2&utm_source=posgradunama&utm_campaign=especializaçãoemdireitodotrabalhoprevidenciário&coupon=UNAMA6",
+            "parcelado": "?utm=pos-aovivo-unama-cameta8&utm_source=posgradunama&utm_campaign=especializaçãoemdireitodotrabalhoprevidenciário"
+        },
+        "ao-vivo-mba-em-gestao-de-pessoas": {
+            "a-vista": "?utm=pos-aovivo-unama-cameta-3&utm_source=posgradunama&utm_campaign=mbaemgestãodepessoas&coupon=UNAMA7",
+            "parcelado": "?utm=pos-aovivo-unama-cameta9&utm_source=posgradunama&utm_campaign=mbaemgestãodepessoas"
+        },
+        "ao-vivo-especializacao-em-gestao-de-departamento-pessoal-e-legislacao-trabalhista": {
+            "a-vista": "?utm=pos-aovivo-unama-cameta&utm_source=posgradunama&utm_campaign=especializaçãoemgestãodedepartamentopessoalelegislaçãotrabalhista&coupon=UNAMA4",
+            "parcelado": "?utm=pos-aovivo-unama-cameta6&utm_source=posgradunama&utm_campaign=especializaçãoemgestãodedepartamentopessoalelegislaçãotrabalhista"
+        },
+        "ao-vivo-especializacao-em-psicologia-organizacional-e-do-trabalho": {
+            "a-vista": "?utm=pos-aovivo-unama-cameta4&utm_source=posgradunama&utm_campaign=especializaçãoempsicologiaorganizacionaledotrabalho&coupon=UNAMA8",
+            "parcelado": "?utm=pos-aovivo-unama-cameta10&utm_source=posgradunama&utm_campaign=especializaçãoempsicologiaorganizacionaledotrabalho"
+        },
+        "ao-vivo-especializacao-em-tea-transtorno-do-espectro-autista": {
+            "a-vista": "?utm=pos-aovivo-unama-cameta-2&utm_source=posgradunama&utm_campaign=especializaçãoemtea–transtornodoespectroautista&coupon=UNAMA6",
+            "parcelado": "?utm=pos-aovivo-unama-cameta8&utm_source=posgradunama&utm_campaign=especializaçãoemtea–transtornodoespectroautista"
+        },
+        "mba-em-gestao-de-pessoas": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=mba em gestão de pessoas&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=mba%20em%20gest%C3%A3o%20de%20pessoas"
+        },
+        "mba-em-gestao-publica": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=mba%20em%20gest%C3%A3o%20p%C3%BAblica&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=mba%20em%20gest%C3%A3o%20p%C3%BAblica"
+        },
+        "mba-em-recursos-humanos": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=mba%20em%20recursos%20humanos&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=mba%20em%20recursos%20humanos"
+        },
+        "especializacao-em-gestao-da-qualidade-e-auditoria": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20gest%C3%A3o%20da%20qualidade%20e%20auditoria&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20gest%C3%A3o%20da%20qualidade%20e%20auditoria"
+        },
+        "mba-em-coaching-e-gestao-de-pessoas": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=mba%20em%20coaching%20e%20gest%C3%A3o%20de%20pessoas&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=mba%20em%20coaching%20e%20gest%C3%A3o%20de%20pessoas"
+        },
+        "mba-em-gestao-de-projetos": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=mba em gestão de projetos&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=mba em gestão de projetos"
+        },
+        "mba-em-contabilidade-auditoria-e-controladoria": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=mba em contabilidade, auditoria  e controladoria&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=mba em contabilidade, auditoria  e controladoria"
+        },
+        "especializacao-em-engenharia-de-producao": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=especialização em engenharia de produção &coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=especialização em engenharia de produção"
+        },
+        "mba-em-gestao-comercial-e-de-marketing": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=mba em gestão comercial e de marketing&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=mba em gestão comercial e de marketing"
+        },
+        "mba-em-administracao-publica": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=mba em administração pública&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=mba em administração pública"
+        },
+        "psicologia-organizacional": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=especialização em psicologia organizacional&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=especialização em psicologia organizacional"
+        },
+        "especializacao-em-educacao-especial-e-inclusiva": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=especializaçãoemeducaçãoespeciale%20inclusiva&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=especializaçãoemeducaçãoespecialeinclusiva"
+        },
+        "especializacao-em-gestao-escolar": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=especialização em gestão escolar&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=especializaçãoemgestãoescolar"
+        },
+        "especializacao-em-educacao-infantil-e-alfabetizacao": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=especialização em educação infantil e alfabetização&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=especialização em educação infantil e alfabetização"
+        },
+        "especializacao-em-psicopedagogia-na-escola": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=especialização em psicopedagogia na escola&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=especialização empsicopedagogianaescola"
+        },
+        "especializacao-em-aulas-on-line-para-educacao-basica": {
+            "a-vista": "?utm=pos-grad-unama-cameta-1&utm_source=posgradunama&utm_campaign=especialização em aulas on-line para educação básica&coupon=UNAMA3",
+            "parcelado": "?utm=pos-grad-unama-cameta-4&utm_source=posgradunama&utm_campaign=especialização em aulas on-line para educação básica"
+        },
+        "especializacao-em-educacao-infantil": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=especialização em educação infantil &coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=especialização em educação infantil"
+        },
+        "especializacao-em-educacao-em-meio-ambiente": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=especialização em educação em meio ambiente&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=especialização em educação em meio ambiente"
+        },
+        "especializacao-em-educacao-especial": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=especialização em educação especial&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=especialização em educação especial"
+        },
+        "especializacao-em-neuroeducacao": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=especialização em neuroeducação&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=especialização em neuroeducação"
+        },
+        "especializacao-em-alfabetizacao-e-letramento": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=especialização em alfabetização e letramento&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=especialização em alfabetização e letramento"
+        },
+        "especializacao-em-praticas-de-letramento-e-alfabetizacao": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=especialização em práticas de letramento e alfabetização&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=especialização em práticas de letramento e alfabetização"
+        },
+        "especializacao-em-docencia-do-ensino-superior": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=especialização em docência do ensino superior&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=especialização em docência do ensino superior"
+        },
+        "especializacao-em-saude-publica": {
+            "a-vista": "?utm=pos-grad-unama-cameta-2&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20sa%C3%BAde%20p%C3%BAblica&coupon=UNAMA01",
+            "parcelado": "?utm=pos-grad-unama-cameta-5&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20sa%C3%BAde%20p%C3%BAblica"
+        },
+        "especializacao-em-saude-publica-e-coletiva": {
+            "a-vista": "?utm=pos-grad-unama-cameta-2&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20sa%C3%BAde%20p%C3%BAblica%20e%20coletiva&coupon=UNAMA01",
+            "parcelado": "?utm=pos-grad-unama-cameta-5&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20sa%C3%BAde%20p%C3%BAblica%20e%20coletiva"
+        },
+        "especializacao-em-enfermagem-oncologica": {
+            "a-vista": "?utm=pos-grad-unama-cameta-2&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20enfermagem%20oncol%C3%B3gica&coupon=UNAMA01",
+            "parcelado": "?utm=pos-grad-unama-cameta-5&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20enfermagem%20oncol%C3%B3gica"
+        },
+        "especializacao-em-farmacia-hospitalar": {
+            "a-vista": "?utm=pos-grad-unama-cameta-2&utm_source=posgradunama&utm_campaign=especialização em farmácia hospitalar&coupon=UNAMA01",
+            "parcelado": "?utm=pos-grad-unama-cameta-5&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20farm%C3%A1cia%20hospitalar"
+        },
+        "especializacao-em-ciencia-de-dados": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20ci%C3%AAncia%20de%20dados&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20ci%C3%AAncia%20de%20dados"
+        },
+        "especializacao-em-seguranca-digital": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20seguran%C3%A7a%20digital&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20seguran%C3%A7a%20digital"
+        },
+        "especializacao-em-data-science": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20data%20science%20&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20data%20science"
+        },
+        "especializacao-em-gerenciamento-de-ti": {
+            "a-vista": "?utm=pos-grad-unama-cameta-2&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20gerenciamento%20de%20ti&coupon=UNAMA01",
+            "parcelado": "?utm=pos-grad-unama-cameta-5&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20gerenciamento%20de%20ti"
+        },
+        "especializacao-em-direito-civil-e-processo-civil": {
+            "a-vista": "?utm=pos-grad-unama-cameta-2&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20direito%20civil%20e%20processo%20civil&coupon=UNAMA01",
+            "parcelado": "?utm=pos-grad-unama-cameta-5&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20direito%20civil%20e%20processo%20civil"
+        },
+        "especializacao-em-direito-publico-e-administracao-publica": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20direito%20p%C3%BAblico%20e%20administra%C3%A7%C3%A3o%20p%C3%BAblica&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20direito%20p%C3%BAblico%20e%20administra%C3%A7%C3%A3o%20p%C3%BAblica"
+        },
+        "especializacao-em-gestao-de-contratos-publicos": {
+            "a-vista": "?utm=pos-grad-unama-cameta&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20gest%C3%A3o%20de%20contratos%20p%C3%BAblicos&coupon=UNAMA2",
+            "parcelado": "?utm=pos-grad-unama-cameta-3&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20gest%C3%A3o%20de%20contratos%20p%C3%BAblicos"
+        },
+        "especializacao-em-direito-ambiental": {
+            "a-vista": "?utm=pos-grad-unama-cameta-2&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20direito%20ambiental%20&coupon=UNAMA01",
+            "parcelado": "?utm=pos-grad-unama-cameta-5&utm_source=posgradunama&utm_campaign=especializa%C3%A7%C3%A3o%20em%20direito%20ambiental"
+        }
+    }
+}
